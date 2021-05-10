26ab6bf519b878040158ca014ecd6bf2811d5301
harshmeet1256 <76517536+harshmeet1256@users.noreply.github.com>
2021-05-10 14:10:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function draw(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata")
     var responseJSON = await response.json();
     var datetime=responseJSON.datetime;
     var hour = datetime.slice(11,13);
